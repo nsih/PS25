4a3e6ea7196a35c61442e59e80c14bf9db0625c9
nsih6571 <nsih6571@gmail.com>
2025-11-12 20:46:33 +0900

    Update 9663 N-Queen.py

--- Files/20. 백트래킹/9663 N-Queen.py
+++ Files/20. 백트래킹/9663 N-Queen.py
@@ -30,3 +30,19 @@ diag1 = set()  # 대각1
 diag2 = set()  # 대각2
 
 print(nq(0))
+
+
+'''
+
+같은 행은 안쓰니까 행(row)를 넘어가며 찾는다.
+
+n이 row고 row 안에 col을 넣어서 체크 -> row 체크는 자동으로 할 필요 없어짐
+
+cols,diag1,2 를 set으로 선언하고 매 행마다 (col in cols) or (row + col in diag1) or (row - col in diag2) 체크
+
+넣으면 기록하고 다음행으로 넘어감
+
+돌아오면 기록지우고 다음 열 체크
+
+
+'''
