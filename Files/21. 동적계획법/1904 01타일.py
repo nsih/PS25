bcd12514510464268d1060bc423813cf4a989723
nsih6571 <nsih6571@gmail.com>
2025-11-17 20:26:58 +0900

    11.16

---
+++ Files/21. 동적계획법/1904 01타일.py
@@ -0,0 +1,12 @@
+import sys
+
+n = int(sys.stdin.readline())
+
+dp = [0] * (max(n + 1, 3))
+dp[1] = 1
+dp[2] = 2
+
+for i in range(3, n + 1):
+    dp[i] = (dp[i-1] + dp[i-2]) % 15746
+
+print(dp[n])
