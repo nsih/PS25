41383e1c9773e6e39a8310bdbdcfe1953c5c9bfc
nsih6571 <nsih6571@gmail.com>
2025-11-23 01:39:20 +0900

    동적계획법 (15/16)

---
+++ Files/21. 동적계획법/2565 전깃줄.py
@@ -0,0 +1,14 @@
+import sys
+
+n = int(sys.stdin.readline())
+lst = [list( map(int, sys.stdin.readline().split())) for _ in range(n)]
+lst.sort(key=lambda x: x[0])
+
+dp = [1] * n
+
+for i in range(1, n):
+    for j in range(i):
+        if lst[i][1] > lst[j][1]:
+            dp[i] = max(dp[i],dp[j] + 1)
+
+print(n-max(dp))
