55e5a02838a33dd85ddcef8afab8934633340ad0
nsih6571 <nsih6571@gmail.com>
2025-11-24 20:15:32 +0900

    11.24
    dp 끝
    누적합 2/6

--- Files/22. 누적합/11659 구간합 구하기 4.py
+++ Files/22. 누적합/11659 구간합 구하기 4.py
@@ -2,12 +2,13 @@ import sys
 
 n, m = map(int, sys.stdin.readline().split())
 lst = list(map(int, sys.stdin.readline().split()))
-prefix_sum = [0]
 
+prefix_sum = [0]
 for i in range(n):
     prefix_sum.append(lst[i] + prefix_sum[-1])
 
-for _ in range(m):
-    a, b = map(int, sys.stdin.readline().split())
+m_sum = []
+for i in range(n - m + 1):
+    m_sum.append(prefix_sum[i+m] - prefix_sum[i])
 
-    print(prefix_sum[b] - prefix_sum[a - 1])
+print(max(m_sum))
