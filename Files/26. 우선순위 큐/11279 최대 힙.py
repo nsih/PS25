0971550789d5fc3da175d4c1d494c102fa8133f2
sih <81147612+nsih@users.noreply.github.com>
2025-12-31 17:57:32 +0900

    우선순위 큐 2/6
    새해복많이 받고 싶다

--- Files/26. 우선순위 큐/11279 최대 힙.py
+++ Files/26. 우선순위 큐/11279 최대 힙.py
@@ -2,19 +2,22 @@ import sys
 import heapq
 
 n = int(sys.stdin.readline())
+
 heap = []
+
 
 for _ in range(n):
     x = int(sys.stdin.readline())
 
     if x == 0:
-        print(heapq.heappop(heap) if heap else 0)
+        print(-heapq.heappop(heap) if heap else 0)
 
     else:
-        heapq.heappush(heap, x)
+        heapq.heappush(heap, -x)
 
 
 '''
 heapq는 최솟값을 꺼낸다.
-그러니까 그냥 최솟값을 꺼낼 수 있다.
+그러니까 부호를 반대로 넣으면 최댓값을 꺼낼수 있다.
+꺼낼때 *-1만 해주자
 '''
